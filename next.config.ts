b3ebff1985e26516de66e26db6888771f5fdba47
zzznly <zzznly@gmail.com>
2024-12-24 11:02:05 +0900

    chore: nextConfig rewrites 설정

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://api.spotify.com/v1/:path*'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
