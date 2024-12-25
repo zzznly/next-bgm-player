d8379629ad4d45d7a574e4cb9a697c6f3996aad8
zzznly <zzznly@gmail.com>
2024-12-25 22:37:01 +0900

    feat: auth callback 페이지 생성

---
+++ src/app/callback/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { saveTokenParams } from "@/utils/auth";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function CallbackPage() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const code = searchParams.get("code");
+  const state = searchParams.get("state");
+
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchToken = async (code: string | null, state: string | null) => {
+      try {
+        if (!code || !state) {
+          setError("Missing code or state.");
+          setLoading(false);
+          return;
+        }
+
+        const response = await fetch(
+          `/api/auth/callback?code=${code}&state=${state}`
+        );
+        const data = await response.json();
+        console.log("# data: ", data);
+        saveTokenParams(data);
+        // router.push("/");
+      } catch (error) {
+        console.error("# error: ", error);
+        setError(error.message);
+        throw error;
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchToken(code, state);
+  }, [searchParams]);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  return <div>Authenticated successfully!</div>;
+}
