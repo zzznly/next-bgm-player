10f908ced302651c1b9f3d821004faa622c46aad
zzznly <zzznly@gmail.com>
2024-12-28 14:17:56 +0900

    MainLayout 스타일 수정

--- src/app/(.)(discover)/layout.tsx
+++ src/app/(.)(discover)/layout.tsx
@@ -3,15 +3,19 @@ import Header from "@/components/header/Header";
 import PlayerPanel from "@/components/player-panel/PlayerPanel";
 import Sidebar from "@/components/sidebar/Sidebar";
 
-export default function MainLayout({ children }: { children: React.ReactNode }) {
-    return <div className={`${styles["layout"]} ${styles["layout-row"]}`}>
-        <Sidebar />
-        <div className={styles["content"]}>
-            <Header />
-            <div className={styles["page"]}>
-                {children}
-            </div>
-        </div>
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className={`${styles["layout"]} ${styles["layout-row"]}`}>
+      <Sidebar />
+      <div className={styles["content"]}>
+        <Header />
+        <div className={styles["page"]}>{children}</div>
         <PlayerPanel />
+      </div>
     </div>
+  );
 }
