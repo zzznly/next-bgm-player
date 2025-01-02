fdf37c4a25efab1015dd5d6c07acb03572c5f67b
zzznly <zzznly@gmail.com>
2025-01-02 22:39:23 +0900

    feat: playlists/new-release api routes

--- src/app/api/playlists/new-release/route.ts
+++ src/app/api/playlists/new-release/route.ts
@@ -3,20 +3,19 @@ import { type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   try {
-    const access_token = request.headers.get("Authorization")?.split(" ")[1];
+    const accessToken = request.headers.get("Authorization")?.split(" ")[1];
     const response = await fetch(
       `${process.env.SPOTIFY_BASE_URL}/browse/new-releases`,
       {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${access_token}`,
+          Authorization: `Bearer ${accessToken}`,
         },
       }
     );
 
     const data = await response.json();
-    console.log("## new-release api response: ", response);
     return NextResponse.json(data);
   } catch (error) {
     return NextResponse.json(
