16e7812d53cb867694a11bf40c23f57f501aa75a
zzznly <zzznly@gmail.com>
2025-01-02 23:00:53 +0900

    feat: Discover page - New Release section

--- src/app/(.)(discover)/page.tsx
+++ src/app/(.)(discover)/page.tsx
@@ -1,3 +1,55 @@
-export default function Discover() {
-  return <div>Discover(Home)</div>;
+import { queryOptions } from "@/service/playlist/queries";
+import { getDehydratedQuery } from "@/utils/react-query";
+import styles from "./page.module.scss";
+
+export default async function Discover() {
+  const { queryKey, queryFn } = queryOptions.newReleaseAlbums();
+  const {
+    state: { data: newReleaseAlbums },
+  } = await getDehydratedQuery({
+    queryKey,
+    queryFn,
+  });
+
+  const getRandomContents = (arr: NewReleaseAlbumItem[] | any[]) => {
+    return arr?.sort(() => 0.5 - Math.random()).slice(0, 5);
+  };
+
+  return (
+    <div className={styles["discover-page"]}>
+      <h2 className={styles["discover-page-title"]}>Discover</h2>
+      <div className={styles["discover-page-content"]}>
+        <div className={styles["section"]}>
+          <div className={styles["section-head"]}>
+            <h2 className={styles["section-title"]}>New Release</h2>
+            {/* <a className={styles["link-more"]}>View All</a> */}
+          </div>
+          <div className={styles["section-content"]}>
+            {getRandomContents(newReleaseAlbums?.albums?.items || [])?.map(
+              (item: NewReleaseAlbumItem) => (
+                <div
+                  className={styles["section-item"]}
+                  style={{
+                    backgroundImage: `url(${item?.images[0].url}), linear-gradient(to top, rgba(77, 77, 86, 0.231372549), rgba(77, 77, 86, 0))`,
+                    backgroundSize: "cover",
+                    backgroundBlendMode: "multiply",
+                  }}
+                  // onClick={() => setPlayingURL(item.uri)}
+                >
+                  <p className={styles["section-item-name"]}>{item?.name}</p>
+                  <p className={styles["section-item-artist"]}>
+                    {item?.artists[0].name}
+                  </p>
+                </div>
+              )
+            )}
+          </div>
+        </div>
+
+        {/* {categoryItems.map((item: CategoriesItem) => (
+          <HomeSection key={item.id} {...item} />
+        ))} */}
+      </div>
+    </div>
+  );
 }
