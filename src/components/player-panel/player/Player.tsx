263e4185603cf92f3d99b89185a3b26cdc987257
zzznly <zzznly@gmail.com>
2025-01-08 15:40:15 +0900

    Player 스타일 수정

--- src/components/player-panel/player/Player.tsx
+++ src/components/player-panel/player/Player.tsx
@@ -4,117 +4,121 @@ import { useState } from "react";
 import styles from "./styles.module.scss";
 
 import {
-    useMutationPlayerPause,
-    useMutationPlayerStart,
-    useMutationSeekPosition,
-    useMutationSetRepeat,
-    useMutationSkipNext,
-    useMutationSkipPrev,
-    useMutationToggleShuffle,
+  useMutationPlayerPause,
+  useMutationPlayerStart,
+  useMutationSeekPosition,
+  useMutationSetRepeat,
+  useMutationSkipNext,
+  useMutationSkipPrev,
+  useMutationToggleShuffle,
 } from "@service/Player/usePlayer";
 import usePlaying from "@store/playing/usePlaying";
 import useSDK from "@store/sdk/useSDK";
 import { convertDurationTime } from "@utils/convert";
 import usePlayerController from "@hooks/usePlayerController";
 import svgIcon from "@/components/svgIcon/SvgIcon";
+import SvgIcon from "@/components/svgIcon/SvgIcon";
 
 const SKIP_PREV_THRESHOLD = 3000;
 const REPEAT_STATES = ["off", "context", "track"];
 
 export default function Player() {
-    return (
-        <div className={styles["player"]}>
-            <h2 className={styles["player-header"]}>NOW PLAYING</h2>
-            <div className={styles["player-body"]}>
-                <div className="player-album">
-                    <img
-                        src={
-                            // currentTrack?.album?.images?.[0]?.url ??
-                            "https://dummyimage.com/200x200/ccc/fff.png"
-                        }
-                        alt="track album"
-                    />
-                </div>
-                <div className="player-track-info">
-                    <p className="player-track-name">
-                        {/* {currentTrack?.name || "No track"} */}
-                    </p>
-                    <p className="player-track-artist">
-                        {/* {currentTrack?.artists?.[0]?.name || "No Track"} */}
-                    </p>
-                </div>
-                <div className="player-bar">
-                    {/* <input
-                        className="player-progress"
-                        name="progress"
-                        type="range"
-                        min={0}
-                        max={durationMs}
-                        value={currentPosition}
-                        onChange={seekToPosition}
-                        step={1000}
-                    /> */}
-                    <div className="player-time">
-                        <div className="player-current-time">
-                            {/* {convertDurationTime(currentPosition)} */}
-                        </div>
-                        <div className="player-duration">
-                            {/* {convertDurationTime(durationMs)} */}
-                        </div>
-                    </div>
-                </div>
-                <div className="player-controller">
-                    <div className="player-controller-left">
-                        {/* <button
-                            className="player-shuffle"
-                            onClick={() => setShuffle(!isShuffle)}
-                        >
-                            <Icon
-                                category="player"
-                                name={isShuffle ? "shuffle-active" : "shuffle"}
-                            />
-                        </button>
-                        <button className="player-skip-prev" onClick={clickPrev}>
-                            <Icon category="player" name="prev" />
-                        </button> */}
-                    </div>
-                    {/* <button
-                        className="player-playpause"
-                        onClick={() =>
-                            isPaused ? onPlay.mutate(currentProgress) : onPause.mutate()
-                        }
-                    >
-                        <Icon category="player" name={isPaused ? "play" : "pause"} />
-                    </button>
-                    <div className="player-controller-right">
-                        <button
-                            className="player-skip-next"
-                            onClick={() => skipNext.mutate()}
-                        >
-                            <Icon category="player" name="next" />
-                        </button>
-                        <button
-                            className="player-repeat"
-                            onClick={() => setRepeatIdx((prev) => prev + 1)}
-                        >
-                            <Icon
-                                category="player"
-                                name={`repeat-${{
-                                    0: "off",
-                                    1: "context",
-                                    2: "track",
-                                }[repeatStateIdx] || "off"
-                                    }`}
-                            />
-                        </button>
-                    </div> */}
-                </div>
-            </div>
-            <div className="player-footer">
-                <div className="player-volume">
-                    <input className="player-volume-input" type="range" />
-                </div>
-            </div>
+  return (
+    <div className={styles["player"]}>
+      <h2 className={styles["player-header"]}>NOW PLAYING</h2>
+      <div className={styles["player-body"]}>
+        <div className={styles["player-album"]}>
+          <img
+            src={
+              // currentTrack?.album?.images?.[0]?.url ??
+              "https://dummyimage.com/200x200/ccc/fff.png"
+            }
+            alt="track album"
+          />
         </div>
-    );
+        <div className={styles["player-track-info"]}>
+          <p className={styles["player-track-name"]}>
+            No Track
+            {/* {currentTrack?.name || "No track"} */}
+          </p>
+          <p className={styles["player-track-artist"]}>
+            No Artist
+            {/* {currentTrack?.artists?.[0]?.name || "No Track"} */}
+          </p>
+        </div>
+        <div className={styles["player-bar"]}>
+          <input
+            className={styles["player-progress"]}
+            name="progress"
+            type="range"
+            min={0}
+            // max={durationMs}
+            // value={currentPosition}
+            // onChange={seekToPosition}
+            step={1000}
+          />
+          <div className={styles["player-time"]}>
+            <div className={styles["player-current-time"]}>
+              {/* {convertDurationTime(currentPosition)} */}
+            </div>
+            <div className={styles["player-duration"]}>
+              {/* {convertDurationTime(durationMs)} */}
+            </div>
+          </div>
+        </div>
+        <div className={styles["player-controller"]}>
+          <div className={styles["player-controller-left"]}>
+            <button
+              className={styles["player-shuffle"]}
+              //   onClick={() => setShuffle(!isShuffle)}
+            >
+              <SvgIcon category="player" name="shuffle" />
+            </button>
+            <button
+              className={styles["player-skip-prev"]}
+              // onClick={clickPrev}
+            >
+              <SvgIcon category="player" name="prev" />
+            </button>
+          </div>
+          <button
+            className={styles["player-playpause"]}
+            // onClick={() =>
+            //   isPaused ? onPlay.mutate(currentProgress) : onPause.mutate()
+            // }
+          >
+            <SvgIcon category="player" name="play" />
+          </button>
+          <div className={styles["player-controller-right"]}>
+            <button
+              className={styles["player-skip-next"]}
+              //   onClick={() => skipNext.mutate()}
+            >
+              <SvgIcon category="player" name="next" />
+            </button>
+            <button
+              className={styles["player-repeat"]}
+              //   onClick={() => setRepeatIdx((prev) => prev + 1)}
+            >
+              <SvgIcon
+                category="player"
+                name={`repeat-${
+                  {
+                    0: "off",
+                    1: "context",
+                    2: "track",
+                  }[0] || "off"
+                }`}
+              />
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* <div className={styles["player-footer"]}>
+        <div className={styles["player-volume"]}>
+          <input className={styles["player-volume-input"]} type="range" />
+        </div>
+      </div> */}
+    </div>
+  );
 }
--- src/components/player-panel/playlist/PlayList.tsx
+++ src/components/player-panel/playlist/PlayList.tsx
@@ -1,15 +1,18 @@
+"use client";
+
 import styles from "./styles.module.scss";
 
 export default function PlayerList() {
-    return <div className={styles["playlist"]}>
-        <div className={styles["playlist-wrap"]}>
-            <div className={styles["playlist-current"]}>
-                <p className={styles["playlist-title"]}>Now Playing</p>
-                {/* {currently_playing && <PlayerListItem {...currentTrack} />} */}
-            </div>
-            <div className={styles["playlist-queue"]}>
-                {/* <p className={styles["playlist-title"]}>Next Tracks</p> */}
-                {/* {queue
+  return (
+    <div className={styles["playlist"]}>
+      <div className={styles["playlist-wrap"]}>
+        <div className={styles["playlist-current"]}>
+          <p className={styles["playlist-title"]}>Now Playing</p>
+          {/* {currently_playing && <PlayerListItem {...currentTrack} />} */}
+        </div>
+        <div className={styles["playlist-queue"]}>
+          {/* <p className={styles["playlist-title"]}>Next Tracks</p> */}
+          {/* {queue
                     ?.map((v: any, i: number, self: any[]) => {
                         return self.findIndex((obj: any) => obj.uri === v.uri) === i
                             ? v
@@ -19,7 +22,8 @@ export default function PlayerList() {
                     .map((item: any, idx: number) => (
                         <PlayerListItem key={`track-${idx}`} {...item} />
                     ))} */}
-            </div>
         </div>
-    </div>;
+      </div>
+    </div>
+  );
 }
