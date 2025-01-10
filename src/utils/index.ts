be8eed7e35f570553bc2a82108a51d616208f390
zzznly <zzznly@gmail.com>
2025-01-11 04:02:40 +0900

    feat: detail page

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -43,3 +43,9 @@ export const isEqual = (value: unknown, other: unknown): boolean => {
 
   return value === other;
 };
+
+export const convertTime = (ms: number): string => {
+  const minutes = Math.floor(ms / 60000);
+  const seconds = Math.floor((ms % 60000) / 1000);
+  return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+}
