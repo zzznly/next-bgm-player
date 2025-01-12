62a9022c9951699fb3fc16dc44729145337f8f59
zzznly <zzznly@gmail.com>
2025-01-13 01:30:57 +0900

    global types 추가

---
+++ src/types/auth.d.ts
@@ -0,0 +1,7 @@
+interface TokenParams {
+  access_token: string;
+  token_type: string;
+  scope: string;
+  expires_in: number;
+  refresh_token?: string;
+}
--- src/types/playlist.d.ts
+++ src/types/playlist.d.ts
@@ -1,20 +1,18 @@
+interface PlaylistRequest {
+  playlist_id: string;
+}
+
 interface NewReleaseAlbumsResponse {
-  albums: ListItemsResponse<NewReleaseAlbumItem>;
+  albums: ListDataResponse<NewReleaseAlbumItem>;
 }
 interface NewReleaseAlbumItem {
   album_type: string;
   total_tracks: number;
   available_markets: string[];
-  external_urls: {
-    spotify: string;
-  };
+  external_urls: ExternalUrls;
   href: string;
   id: string;
-  images: {
-    height: number;
-    url: string;
-    width: number;
-  }[];
+  images: Image[];
   name: string;
   release_date: string;
   release_date_precision: string;
@@ -24,193 +22,201 @@ interface NewReleaseAlbumItem {
   type: string;
   uri: string;
   copyrights: any[];
-  external_ids: {
-    spotify: string;
-  };
+  external_ids: ExternalIds;
   genres: string[];
   label: string;
   popularity: number;
   album_group: string;
-  artists: {
+  artists: Artist[];
+}
+
+interface NewArtistsPlaylistResponse {
+  external_urls: ExternalUrls;
+  followers: {
+    href: string;
+    total: number;
+  };
+  genres: string[];
+  href: string;
+  id: string;
+  images: Image[];
+  name: string;
+  popularity: number;
+  type: "artist";
+  uri: string;
+}
+
+interface CategoryPlaylistRequest {
+  category_id: string;
+}
+interface CategoryPlaylistResponse {
+  playlists: {
+    href: string;
+    items: PlaylistItem[];
+  };
+}
+interface PlaylistItem {
+  collaborative: boolean;
+  description: string;
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  images: Image[];
+  name: string;
+  owner: Owner;
+  public: boolean;
+  snapshot_id: string;
+  tracks: Tracks;
+  type: string;
+  uri: string;
+}
+
+interface ArtistPlaylistResponse {
+  artists: ArtistPlaylistItem[];
+}
+interface ArtistPlaylistItem {
+  external_urls: ExternalUrls;
+  followers?: {
+    href: string;
+    total: number;
+  };
+  genres: string[];
+  href: string;
+  id: string;
+  images: Image[];
+  name: string;
+  popularity: number;
+  type: string;
+  uri: string;
+}
+
+interface TrackItem {
+  album: Album;
+  artists: Artist[];
+  available_markets: string[];
+  disc_number: number;
+  duration_ms: number;
+  explicit: boolean;
+  external_ids: {
+    isrc: string;
+    ean: string;
+    upc: string;
+  };
+  external_urls: {
+    spotify: string;
+  };
+  href: string;
+  id: string;
+  is_playable: boolean;
+  linked_from: {
     external_urls: {
-      isrc: string;
+      spotify: string;
     };
     href: string;
     id: string;
-    name: string;
     type: string;
     uri: string;
+  };
+  restrictions: {
+    reason: string;
+  };
+  name: string;
+  popularity: number;
+  preview_url: string;
+  track_number: number;
+  type: string;
+  uri: string;
+  is_local: boolean;
+}
+
+interface AlbumDetailResponse {
+  album_type: string;
+  total_tracks: number;
+  available_markets: string[];
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  images: Image[];
+  name: string;
+  release_date: string;
+  release_date_precision: string;
+  restrictions?: {
+    reason: string;
+  };
+  type: string;
+  uri: string;
+  artists: Artist[];
+  tracks: ListDataResponse<PlaylistDetailItemsTrack>;
+  copyrights: {
+    text: string;
+    type: string;
   }[];
+  external_ids: {
+    isrc: string;
+    ean: string;
+    upc: string;
+  };
+  genres: string[];
+  label: string;
+  popularity: number;
 }
 
-interface PlaylistRequest {
-  playlist_id: string;
+interface PlaylistDetailResponse {
+  collaborative: boolean;
+  description: string;
+  external_urls: ExternalUrls;
+  followers: {
+    href: string | null;
+    total: number;
+  };
+  href: string;
+  id: string;
+  images: Image[];
+  name: string;
+  owner: Owner;
+  primary_color: string | null;
+  public: boolean | null;
+  snapshot_id: string;
+  tracks: ListDataResponse<PlaylistDetailItem>;
+  type: string;
+  uri: string;
 }
-
-//
-// interface TrackItem {
-//   album: Album;
-//   artists: Artist[];
-//   available_markets: string[];
-//   disc_number: number;
-//   duration_ms: number;
-//   explicit: boolean;
-//   external_ids: {
-//     isrc: string;
-//     ean: string;
-//     upc: string;
-//   };
-//   external_urls: {
-//     spotify: string;
-//   };
-//   href: string;
-//   id: string;
-//   is_playable: boolean;
-//   linked_from: {
-//     external_urls: {
-//       spotify: string;
-//     };
-//     href: string;
-//     id: string;
-//     type: string;
-//     uri: string;
-//   };
-//   restrictions: {
-//     reason: string;
-//   };
-//   name: string;
-//   popularity: number;
-//   preview_url: string;
-//   track_number: number;
-//   type: string;
-//   uri: string;
-//   is_local: boolean;
-// }
-
-// interface PlaylistDetailResponse {
-//   collaborative: boolean;
-//   description: string;
-//   external_urls: {
-//     spotify: string;
-//   };
-//   followers: {
-//     href: string | null;
-//     total: number;
-//   };
-//   href: string;
-//   id: string;
-//   images: {
-//     height: number;
-//     url: string;
-//     width: number;
-//   }[];
-//   name: string;
-//   owner: {
-//     display_name: string;
-//     external_urls: {
-//       spotify: string;
-//     };
-//     href: string;
-//     id: string;
-//     type: string;
-//     uri: string;
-//   };
-//   primary_color: string | null;
-//   public: boolean | null;
-//   snapshot_id: string;
-//   tracks: {
-//     href: string;
-//     items: PlaylistDetailItems[];
-//     limit: number;
-//     next: string | null;
-//     offset: number;
-//     previous: string | null;
-//     total: number;
-//   };
-//   type: string;
-//   uri: string;
-// }
-// interface PlaylistDetailItems {
-//   added_at: string;
-//   added_by: {
-//     external_urls: {
-//       spotify: string;
-//     };
-//     href: string;
-//     id: string;
-//     type: string;
-//     uri: string;
-//   };
-//   is_local: boolean;
-//   primary_color: null | string;
-//   track: PlaylistDetailItemsTrack;
-//   video_thumbnail: {
-//     url: null | string;
-//   };
-// }
-// interface PlaylistDetailItemsTrack {
-//   album: {
-//     album_group: string;
-//     album_type: string;
-//     artists: {
-//       external_urls: {
-//         spotify: string;
-//       };
-//     }[];
-//     external_urls: {
-//       spotify: string;
-//     };
-//     href: string;
-//     id: string;
-//     images: Image[];
-//     is_playable: boolean;
-//     name: string;
-//     release_date: string;
-//     release_date_precision: string;
-//     total_tracks: number;
-//     type: string;
-//     uri: string;
-//   };
-//   artists: Artist[];
-//   disc_number: number;
-//   duration_ms: number;
-//   episode: boolean;
-//   explicit: boolean;
-//   external_ids: {
-//     isrc: string;
-//   };
-//   external_urls: {
-//     spotify: string;
-//   };
-//   href: string;
-//   id: string;
-//   is_local: boolean;
-//   is_playable: boolean;
-//   name: string;
-//   popularity: number;
-//   preview_url: string;
-//   track: boolean;
-//   track_number: number;
-//   type: string;
-//   uri: string;
-// }
-
-// interface FeaturedPlaylistRes {
-//   message: string;
-//   playlists: FeaturedPlaylistItem[];
-// }
-// interface FeaturedPlaylistItem {
-//   collaborative: boolean;
-//   description: string;
-//   external_urls: ExternalUrls;
-//   href: string;
-//   id: string;
-//   images: Image[];
-//   name: string;
-//   owner: Owner;
-//   public: boolean;
-//   snapshot_id: string;
-//   tracks: Tracks;
-//   type: string;
-//   uri: string;
-// }
+interface PlaylistDetailItem {
+  added_at: string;
+  added_by: {
+    external_urls: ExternalUrls;
+    href: string;
+    id: string;
+    type: string;
+    uri: string;
+  };
+  is_local: boolean;
+  primary_color: null | string;
+  track: PlaylistDetailItemTrack;
+  video_thumbnail: {
+    url: null | string;
+  };
+}
+interface PlaylistDetailItemTrack {
+  album: Album;
+  artists: Artist[];
+  disc_number: number;
+  duration_ms: number;
+  episode: boolean;
+  explicit: boolean;
+  external_ids: {
+    isrc: string;
+  };
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  is_local: boolean;
+  is_playable: boolean;
+  name: string;
+  popularity: number;
+  preview_url: string;
+  track: boolean;
+  track_number: number;
+  type: string;
+  uri: string;
+}
