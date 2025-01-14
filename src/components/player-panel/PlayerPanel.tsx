d69425001af9472accbc7b06a5936612e124b25d
zzznly <zzznly@gmail.com>
2025-01-14 23:30:25 +0900

    style: 스타일 수정

--- src/components/player-panel/PlayerPanel.tsx
+++ src/components/player-panel/PlayerPanel.tsx
@@ -1,10 +1,17 @@
+"use client";
+
 import styles from "./styles.module.scss";
 import PlayList from "./playlist/PlayList";
 import Player from "./player/Player";
+import useSpotifyPlayer from "@/hooks/useSpotifyPlayer";
 
 export default function PlayerPanel() {
-    return <div className={styles["player-panel"]}>
-        <PlayList />
-        <Player />
-    </div>;
+  useSpotifyPlayer();
+
+  return (
+    <div className={styles["player-panel"]}>
+      <PlayList />
+      <Player />
+    </div>
+  );
 }
